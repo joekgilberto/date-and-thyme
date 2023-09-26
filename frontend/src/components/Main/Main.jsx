7a6e449b0f446c336b9b811ec1166ed92df5f44b
joegilberto <joekgilberto@gmail.com>
2023-09-26 09:23:19 -0400

    adds new food and grocery pages

--- frontend/src/components/Main/Main.jsx
+++ frontend/src/components/Main/Main.jsx
@@ -3,7 +3,9 @@ import './Main.css';
 import { Routes, Route } from 'react-router-dom'
 
 import Home from '../../pages/Home/Home';
+import NewFood from '../../pages/NewFood/NewFood';
 import TestApp from '../../pages/tests/TestApp';
+import Error from '../../pages/Error/Error';
 
 export default function Main() {
 
@@ -11,7 +13,9 @@ export default function Main() {
         <main>
             <Routes>
                 <Route path="/" element={<Home />} />
+                <Route path="/fridge/new" element={<NewFood />} />
                 <Route path="/test" element={<TestApp />} />
+                <Route path={"/*"} element={<Error />} />
             </Routes>
         </main>
     );
---
+++ frontend/src/pages/Error/Error.jsx
@@ -0,0 +1,10 @@
+import './Error.css';
+
+export default function Error() {
+
+  return (
+    <div className='Error'>
+        <h2>404 Page Not Found</h2>
+    </div>
+  );
+}
