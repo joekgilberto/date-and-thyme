f38dfbe9ecda95f6391518468d4c25283c31f0fc
joegilberto <joekgilberto@gmail.com>
2023-09-29 14:21:12 -0400

    updates introduction of openai

--- frontend/src/utilities/open-ai/open-ai-api.js
+++ frontend/src/utilities/open-ai/open-ai-api.js
@@ -1,54 +1,47 @@
 import { Configuration, OpenAIApi } from "openai";
 
 const configuration = new Configuration({
-  apiKey: process.env.REACT_APP_OPENAI_API_KEY,
+    apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
-export default async function aiGeneration(req, res) {
-  if (!configuration.apiKey) {
-    res.status(500).json({
-      error: {
-        message: "OpenAI API key not configured, please follow instructions in README.md",
-      }
-    });
-    return;
-  }
-
-  const animal = req.body.ingredient || '';
-  if (animal.trim().length === 0) {
-    res.status(400).json({
-      error: {
-        message: "No recipe suggestions",
-      }
-    });
-    return;
-  }
-
-  try {
-    const completion = await openai.createCompletion({
-      model: "gpt-3.5-turbo",
-      prompt: prompt(ingredient),
-      temperature: 0.4,
-    });
-    console.log(completion)
-    res.status(200).json({ result: completion.data.choices[0].text });
-  } catch (error) {
-    // Consider adjusting the error handling logic for your use case
-    if (error.response) {
-      console.error(error.response.status, error.response.data);
-      res.status(error.response.status).json(error.response.data);
-    } else {
-      console.error(`Error with OpenAI API request: ${error.message}`);
-      res.status(500).json({
-        error: {
-          message: 'An error occurred during your request.',
+export default async function aiGeneration(input) {
+    if (!configuration.apiKey) {
+        const error = {
+            message: "OpenAI API key not configured, please follow instructions in README.md",
         }
-      });
+        return error
+            ;
     }
-  }
+
+    const ingredient = input || '';
+    if (ingredient.trim().length === 0) {
+        const error = {
+            message: "No recipe suggestions",
+        }
+        return error
+    }
+
+    try {
+        const completion = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: prompt(ingredient),
+            temperature: 0.6,
+        });
+        console.log(completion)
+        const result = completion.data.choices[0].text
+        return result
+    } catch (error) {
+        // Consider adjusting the error handling logic for your use case
+        if (error.response) {
+            console.error(error.response.status, error.response.data);
+        } else {
+            console.error(`Error with OpenAI API request: ${error.message}`);
+        }
+    }
 }
 
+
 function prompt(ingredient) {
-  return `Please suggest one recipe to make with this ingredient: ${ingredient}`;
+    return `Please suggest one recipe to make with this ingredient: ${ingredient}`;
 }
