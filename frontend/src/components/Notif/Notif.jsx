bcd1840351a4e17aa6a3b325221f000ea3ef8e2d
joegilberto <joekgilberto@gmail.com>
2023-09-29 12:25:27 -0400

    adds margin to show food page and edits title of expired severity alerts

--- frontend/src/components/Notif/Notif.jsx
+++ frontend/src/components/Notif/Notif.jsx
@@ -22,7 +22,7 @@ export default function Notif({ notif }) {
     return (
         <div className='Notif'>
             <Alert onClick={() => handleClick(notif)} style={{ fontSize: '16px' }} severity={notif.read ? "info" : notif.days_left > 2 ? "info" : notif.days_left <= 2 && notif.days_left > 0 ? "warning" : "error"}>
-                <AlertTitle>{notif.days_left > 2 ? "REMINDER" : notif.days_left <= 2 && notif.days_left > 0 ? "WARNING" : "ERROR"}</AlertTitle>
+                <AlertTitle>{notif.days_left > 2 ? "REMINDER" : notif.days_left <= 2 && notif.days_left > 0 ? "WARNING" : "EXPIRED"}</AlertTitle>
                 <Link to={`/fridge/${notif.food_item}`}>
                     {notif.days_left > 0 ? (
                         <p>Your {notif.food_item_name} has {notif.days_left} day{notif.days_left !== 1 ? 's' : ''} left.</p>
--- frontend/src/components/ShowNotif/ShowNotif.jsx
+++ frontend/src/components/ShowNotif/ShowNotif.jsx
@@ -8,7 +8,7 @@ export default function ShowNotif({ notif, foodItem }) {
     return (
         <div className='ShowNotif'>
             <Alert style={{ fontSize: '16px' }} severity={notif.days_left > 2 ? "info" : notif.days_left <= 2 && notif.days_left > 0 ? "warning" : "error"}>
-                <AlertTitle>{notif.days_left > 2 ? "REMINDER" : notif.days_left <= 2 && notif.days_left > 0 ? "WARNING" : "ERROR"}</AlertTitle>
+                <AlertTitle>{notif.days_left > 2 ? "REMINDER" : notif.days_left <= 2 && notif.days_left > 0 ? "WARNING" : "EXPIRED"}</AlertTitle>
                 {notif.days_left > 0 ? (
                     <p>Your {notif.food_item_name} has {notif.days_left} day{notif.days_left !== 1 ? 's' : ''} left.</p>
                 ) : (
