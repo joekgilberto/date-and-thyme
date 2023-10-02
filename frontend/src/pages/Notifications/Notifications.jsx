fe0363bf11e0628ccf76fde6b1f5d857ed84d316
joegilberto <joekgilberto@gmail.com>
2023-10-01 20:07:11 -0400

    adds no notifications message if no active notifications

--- frontend/src/pages/Notifications/Notifications.jsx
+++ frontend/src/pages/Notifications/Notifications.jsx
@@ -1,6 +1,6 @@
 import './Notifications.css';
 
-import { useContext, useEffect } from "react";
+import { useState, useContext, useEffect } from "react";
 import { FridgeContext } from "../../data";
 
 import Notif from '../../components/Notif/Notif';
@@ -10,11 +10,22 @@ import Stack from '@mui/material/Stack';
 export default function Notifications() {
 
     const { handleRefresh, notifs } = useContext(FridgeContext);
+    const [count, setCount] = useState(0)
 
     useEffect(() => {
         handleRefresh()
         console.log(notifs)
     }, [])
+
+    useEffect(() => {
+        if (notifs) {
+            for (let n of notifs) {
+                if (n.days_left <= 5) {
+                    setCount(count + 1)
+                }
+            }
+        }
+    }, [notifs])
 
     return (
         <div className='Notifications'>
@@ -25,7 +36,10 @@ export default function Notifications() {
             <Paper>
                 <Stack sx={{ width: '100%', padding: '20px' }} spacing={2}>
                     {notifs?.length ? notifs.map((notif, idx) => {
-                        return (notif.days_left<=5?<Notif key={idx} notif={notif} />: null)
+                        return (<>
+                            {!count ? <h2>no notifiactions</h2> : null}
+                            {notif.days_left <= 5 ? <Notif key={idx} notif={notif} /> : null}
+                        </>)
                     }) : (
                         <h2>no notifiactions</h2>
                     )}
