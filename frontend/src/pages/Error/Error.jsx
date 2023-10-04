939f15a287a9d76e12ba566b09e3d1d5317fd238
joegilberto <joekgilberto@gmail.com>
2023-10-04 12:17:43 -0400

    styles 404 page

--- frontend/src/pages/Error/Error.jsx
+++ frontend/src/pages/Error/Error.jsx
@@ -1,10 +1,22 @@
 import './Error.css';
 
+import { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import { FridgeContext } from "../../data";
+import Paper from '@mui/material/Paper';
+import Button from '@mui/material/Button';
+
 export default function Error() {
+  const { Mooli, OpenSans } = useContext(FridgeContext);
 
   return (
     <div className='Error'>
-        <h1>404 Page Not Found</h1>
+      <Paper elevation={3} style={{ ...OpenSans, width: '50%', padding: '20px 30px' }}>
+        <h1>Error 404 - Page Not Found</h1>
+        <Link to="/">
+          <Button variant='contained' style={{ ...Mooli }}>Return Home</Button>
+        </Link>
+      </Paper>
     </div>
   );
 }
