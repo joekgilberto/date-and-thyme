642ea078876ece3a78eaa19ab6aef88b23ee6b21
joekgilberto <joekgilberto@gmail.com>
2023-12-09 14:21:13 -0500

    adds postgresql to footer

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
 
   return (
     <footer>
-      <p><a href='https://github.com/joekgilberto/date-and-thyme' target='_blank'>date & thyme</a> is made with <a href='https://www.djangoproject.com/' target='_blank'>Django</a>, <a href='https://react.dev/' target='_blank'>React</a>, and <a href='https://mui.com/material-ui/' target='_blank'>Material UI</a> by <a href='https://joekgilberto.com/' target='_blank'>Joe Gilberto</a>.</p>
+      <p><a href='https://github.com/joekgilberto/date-and-thyme' target='_blank'>date & thyme</a> is made with <a href='https://www.djangoproject.com/' target='_blank'>Django</a>, <a href='https://react.dev/' target='_blank'>React</a>, <a href='https://mui.com/material-ui/' target='_blank'>Material UI</a>, and <a href='https://www.postgresql.org/'>PostgreSQL</a> by <a href='https://joekgilberto.com/' target='_blank'>Joe Gilberto</a>.</p>
     </footer>
   );
 }
